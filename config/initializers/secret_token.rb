6d06a92cd80b2e3ed099fdcfb68fa80e15a22bb6
Your Name <senthil.vijay.kumar@gmail.com>
2011-10-22 13:33:45 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whackbackend::Application.config.secret_token = 'f1d8551e3f992d6e4a2718b3f67da0973d77de689c55aa711629b3356e8e576fd270f654c9fa56941711cfca5a79dd23952085afb409cba6139d84b58ee6abd3'
