66ae3d07a221161730144047efc612ffa5cefbe9
Sadique Ali <sdqali@bihzad.(none)>
2011-10-23 02:18:38 +0530

    Better user flow.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    if current_user
+      redirect_to "/complaint/index"
+    end
   end
 
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to "/complaint/index", :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
@@ -15,6 +18,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to "/log_in", :notice => "Logged out!"
   end
 end
